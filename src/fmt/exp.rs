0148d797a1299fc94dfcf0bb1a71955aeacc7424
lazymio <mio@lazym.io>
2026-01-10 17:11:03 +0000

    fix

--- src/fmt/exp.rs
+++ src/fmt/exp.rs
@@ -38,20 +38,6 @@ pub fn to_exp_notation(value: U256, precision: usize, trim_end_zeros: bool, sign
 /// Formats a U256 number to string, adding an exponential notation _hint_ if it
 /// is larger than `10_000`, with a precision of `4` figures, and trimming the
 /// trailing zeros.
-///
-/// # Examples
-///
-/// ```
-/// use alloy_primitives::U256;
-/// use foundry_common_fmt::format_uint_exp as f;
-///
-/// # yansi::disable();
-/// assert_eq!(f(U256::from(0)), "0");
-/// assert_eq!(f(U256::from(1234)), "1234");
-/// assert_eq!(f(U256::from(1234567890)), "1234567890 [1.234e9]");
-/// assert_eq!(f(U256::from(1000000000000000000_u128)), "1000000000000000000 [1e18]");
-/// assert_eq!(f(U256::from(10000000000000000000000_u128)), "10000000000000000000000 [1e22]");
-/// ```
 pub fn format_uint_exp(num: U256) -> String {
     if num < U256::from(10_000) {
         return num.to_string();
@@ -64,29 +50,6 @@ pub fn format_uint_exp(num: U256) -> String {
 /// Formats a U256 number to string, adding an exponential notation _hint_.
 ///
 /// Same as [`format_uint_exp`].
-///
-/// # Examples
-///
-/// ```
-/// use alloy_primitives::I256;
-/// use foundry_common_fmt::format_int_exp as f;
-///
-/// # yansi::disable();
-/// assert_eq!(f(I256::try_from(0).unwrap()), "0");
-/// assert_eq!(f(I256::try_from(-1).unwrap()), "-1");
-/// assert_eq!(f(I256::try_from(1234).unwrap()), "1234");
-/// assert_eq!(f(I256::try_from(1234567890).unwrap()), "1234567890 [1.234e9]");
-/// assert_eq!(f(I256::try_from(-1234567890).unwrap()), "-1234567890 [-1.234e9]");
-/// assert_eq!(f(I256::try_from(1000000000000000000_u128).unwrap()), "1000000000000000000 [1e18]");
-/// assert_eq!(
-///     f(I256::try_from(10000000000000000000000_u128).unwrap()),
-///     "10000000000000000000000 [1e22]"
-/// );
-/// assert_eq!(
-///     f(I256::try_from(-10000000000000000000000_i128).unwrap()),
-///     "-10000000000000000000000 [-1e22]"
-/// );
-/// ```
 pub fn format_int_exp(num: I256) -> String {
     let (sign, abs) = num.into_sign_and_abs();
     if abs < U256::from(10_000) {
