6ab2b4f209d361584971dbcf425c2753abeedc90
lazymio <mio@lazym.io>
2026-01-13 17:11:22 +0000

    bump

--- src/block.rs
+++ src/block.rs
@@ -1,7 +1,8 @@
+use alloy::network::{TransactionResponse, primitives::HeaderResponse};
 use alloy_chains::Chain;
 use revm::{
     context::{BlockEnv, CfgEnv},
-    primitives::hardfork::SpecId,
+    primitives::{U256, hardfork::SpecId},
 };
 
 pub fn empty_blockenv() -> BlockEnv {
@@ -16,4 +17,21 @@ pub fn testing_env(chain: Chain) -> CfgEnv {
     cfg_env.spec = SpecId::default();
 
     cfg_env
+}
+
+pub fn block_env_from_rpc<T, H>(block: alloy::rpc::types::Block<T, H>) -> BlockEnv
+where
+    H: HeaderResponse,
+    T: TransactionResponse,
+{
+    let mut env = empty_blockenv();
+    env.beneficiary = block.header.beneficiary();
+    env.difficulty = block.header.difficulty();
+    env.gas_limit = block.header.gas_limit();
+    env.number = U256::from(block.header.number());
+    env.prevrandao = block.header.mix_hash();
+    env.timestamp = U256::from(block.header.timestamp());
+    env.basefee = block.header.base_fee_per_gas().unwrap_or_default();
+
+    env
 }
