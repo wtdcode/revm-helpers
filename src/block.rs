0520cef850ead340c09b6ef8f910624cb7fff687
lazymio <mio@lazym.io>
2026-01-19 15:01:10 +0000

    bump

--- src/block.rs
+++ src/block.rs
@@ -1,8 +1,11 @@
-use alloy::network::{TransactionResponse, primitives::HeaderResponse};
+use alloy::{
+    consensus::transaction::Recovered,
+    network::{TransactionResponse, primitives::HeaderResponse},
+};
 use alloy_chains::Chain;
 use revm::{
-    context::{BlockEnv, CfgEnv},
-    primitives::{U256, hardfork::SpecId},
+    context::{BlockEnv, CfgEnv, TxEnv},
+    primitives::{TxKind, U256, hardfork::SpecId},
 };
 
 pub fn empty_blockenv() -> BlockEnv {
@@ -19,19 +22,50 @@ pub fn testing_env(chain: Chain) -> CfgEnv {
     cfg_env
 }
 
-pub fn block_env_from_rpc<T, H>(block: alloy::rpc::types::Block<T, H>) -> BlockEnv
+pub fn block_env_from_rpc<H>(block_header: &H) -> BlockEnv
 where
     H: HeaderResponse,
-    T: TransactionResponse,
 {
     let mut env = empty_blockenv();
-    env.beneficiary = block.header.beneficiary();
-    env.difficulty = block.header.difficulty();
-    env.gas_limit = block.header.gas_limit();
-    env.number = U256::from(block.header.number());
-    env.prevrandao = block.header.mix_hash();
-    env.timestamp = U256::from(block.header.timestamp());
-    env.basefee = block.header.base_fee_per_gas().unwrap_or_default();
+    env.beneficiary = block_header.beneficiary();
+    env.difficulty = block_header.difficulty();
+    env.gas_limit = block_header.gas_limit();
+    env.number = U256::from(block_header.number());
+    env.prevrandao = block_header.mix_hash();
+    env.timestamp = U256::from(block_header.timestamp());
+    env.basefee = block_header.base_fee_per_gas().unwrap_or_default();
 
+    env
+}
+
+pub fn tx_env_from_rpc<T>(tx: &Recovered<T>) -> TxEnv
+where
+    T: alloy::consensus::Transaction,
+{
+    let mut env = TxEnv::default();
+    env.caller = tx.signer();
+    env.nonce = tx.nonce();
+    env.gas_limit = tx.gas_limit();
+    env.gas_price = tx.gas_price().unwrap_or_default();
+    env.gas_priority_fee = tx.max_priority_fee_per_gas();
+    env.max_fee_per_blob_gas = tx.max_fee_per_blob_gas().unwrap_or_default();
+    env.data = tx.input().clone();
+    env.kind = if let Some(target) = tx.to() {
+        TxKind::Call(target)
+    } else {
+        TxKind::Create
+    };
+    env.chain_id = tx.chain_id();
+    env.tx_type = tx.ty();
+    env.value = tx.value();
+    env.access_list = tx.access_list().map(|v| v.clone()).unwrap_or_default();
+    env.authorization_list = tx
+        .authorization_list()
+        .map(|v| v.iter().map(|v| Err(v.clone()).into()).collect())
+        .unwrap_or_default();
+    env.blob_hashes = tx
+        .blob_versioned_hashes()
+        .map(|v| v.to_vec())
+        .unwrap_or_default();
     env
 }
